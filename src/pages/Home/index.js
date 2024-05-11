4f46ce522cdb21ca82d8e11c7121926ecb623d07
Eric Wang <eric.wang@theomnieyes.com>
2024-05-11 12:53:09 +0800

    Add logo in NavigationBar and HomePage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,27 @@
+import { Flex, Image } from "antd";
+
+import "./index.css";
+
+
 const Home = () => {
-    return <div>
-        <h1>Home Page</h1>
-    </div>
+    return (
+        <Flex 
+            justify="center" 
+            align="center"
+            style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)"
+            }}
+        >
+            <Image
+                width={"10rem"}
+                preview={false}
+                src="http://44.239.130.59:5000/images/omnieyes_logo.png"
+            />
+        </Flex>
+    )
 }
 
 export default Home;
