27d3e6f9b5ec1084f8892ad83b56bf56b638fc33
Eric Wang <eric.wang@theomnieyes.com>
2024-05-18 11:07:57 +0800

    Add space between avatar and menu list

--- src/global/components/NavigationBar.js
+++ src/global/components/NavigationBar.js
@@ -1,4 +1,4 @@
-import { Layout, Menu, Image } from "antd";
+import { Layout, Menu, Image, Space } from "antd";
 import { Link } from "react-router-dom";
 
 const { Header } = Layout
@@ -7,32 +7,33 @@ const { Header } = Layout
 const NavigationBar = () => {
     return <div>
         <Header style={{ display: 'flex', alignItems: 'center' }}>
-            {/* <div className="demo-logo" /> */}
-            <Image 
-                width={100}
-                onPreview={() => {}}
-                forceRender={false}
-                preview={false}
-                src="https://www.theomnieyes.com/_nuxt/img/omnieyes-logo.d4658d9.svg" 
-            />
-            <Menu
-                theme="dark"
-                mode='horizontal'
-                items={[
-                    {
-                        label: <Link to="/">Home</Link>,
-                        key: ""
-                    },
-                    {
-                        label: <Link to="/event">Event</Link>,
-                        key: "event"
-                    },
-                    {
-                        label: <Link to="/tools">Tools</Link>,
-                        key: "tools"
-                    },
-                ]}
-            />
+            <Space>
+                <Image 
+                    width={100}
+                    onPreview={() => {}}
+                    forceRender={false}
+                    preview={false}
+                    src="https://www.theomnieyes.com/_nuxt/img/omnieyes-logo.d4658d9.svg" 
+                />
+                <Menu
+                    theme="dark"
+                    mode='horizontal'
+                    items={[
+                        {
+                            label: <Link to="/">Home</Link>,
+                            key: ""
+                        },
+                        {
+                            label: <Link to="/event">Event</Link>,
+                            key: "event"
+                        },
+                        {
+                            label: <Link to="/tools">Tools</Link>,
+                            key: "tools"
+                        },
+                    ]}
+                />
+            </Space>
         </Header>
     </div>
 }
